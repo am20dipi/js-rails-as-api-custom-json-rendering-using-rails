ca68a4bac57364b31e48b287ddf30f9f8736e01b
Adriana <am20dipi@alum.siena.edu>
2021-05-21 15:50:29 -0400

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,15 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render json: @birds
+    birds = Bird.all
+    render json: birds
+  end
+
+  def show 
+    bird = Bird.find_by(id: params[:id])
+    render json: {id: bird.id, name: bird.name, species: bird.species }
+    # we are allowing only specific attributes of a bird instance to be shown.
+    # we create a hash using three keys and assign the keys manually with the attributes of bird. 
+    # you can also use .slice(:id, :name, :species) to do the same thing
+    # you can also use only: [] or except: []
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get '/birds' => 'birds#index'
+  get '/birds/:id' => 'birds#show'
+  # get route to birds/:id, from the birds controller and bird controller show action
 end
